a8e9a0fbef685c4a7ad91b7b282c899ffaf71ec0
PremiumScripts <iceyaveri@gmail.com>
2025-08-12 21:35:13 +0800

    Create Luxuria.lua

---
+++ INCB/Luxuria.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/fensi12/Age-Leveler/refs/heads/main/new/main/Booster.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/fensi12/INCB/refs/heads/main/Lux.lua"))()
