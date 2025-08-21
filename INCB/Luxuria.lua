56c99562799705ba44e8cad5664a1221c53c6646
PremiumScripts <iceyaveri@gmail.com>
2025-08-21 19:26:39 +0800

    Update Luxuria.lua

--- INCB/Luxuria.lua
+++ INCB/Luxuria.lua
@@ -1,3 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/fensi12/Age-Leveler/refs/heads/main/new/main/Booster.lua"))()
-ID="f93d3e66-b2e0-4645-a318-a6011edc55b8";
-loadstring(game:HttpGet("https://cdn.reverse.software/loader.luau"))()
+ID="f93d3e66-b2e0-4645-a318-a6011edc55b8";loadstring(game:HttpGet("https://cdn.reverse.software/loader.luau"))()
